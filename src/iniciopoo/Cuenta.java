954485395e40fe7a15ae9ddca187a0806c1f6ff3
sebas <sebas@HP-Sebas>
2024-05-17 11:56:04 +0200

    El primer commit fue satisfactorio. Se ha realizado la preparacion para generar el JavaDoc. Se ha practicado para el examen de entornos

--- src/iniciopoo/Cuenta.java
+++ src/iniciopoo/Cuenta.java
@@ -1,66 +1,101 @@
-
 package iniciopoo;
 
 /*1) Crea una clase llamada Cuenta que tendra los siguientes atributos: titular y cantidad(puede tener decimales)*/
+
+/**
+ * Clase Cuenta que simula la cuenta bancaria
+ * @author sebas
+ * @version 1.0
+ */
+
 public class Cuenta {
+
     private String titular;
     private double cantidad;
-    
+    private String serial;
+
     //El titular sera obligatorio u la cantidad opcional
     //Crea dos contstructores que cumplan lo anterior
-    
-    public Cuenta(String titular){
+
+    /**
+     * Metodo para crear Cuenta
+     * @param titular Nombre y apellido del titular
+     * @param serial Serial, para en un futuro asignar un numero de serie
+     */
+    public Cuenta(String titular, String serial) {
         this.titular = titular;
+        this.serial=serial;
     }
-    
-    public Cuenta(String titular, double cantidad){
+
+    /**
+     * Metodo sobrecargado para crear cuenta 
+     * @param titular Titular de la cuenta
+     * @param cantidad Cantidad de dinero en la cuenata
+     */
+    public Cuenta(String titular, double cantidad) {
         this.titular = titular;
         this.cantidad = cantidad;
     }
-    
+
+    /**
+     * Metodo para sobreescribir el metodo String
+     * @return Devuelve la consulta con un formato especfico
+     */
     @Override
-    public String toString(){
-        return "Titular: "+ getTitular() +" | Cantidad: "+getCantidad();
+    public String toString() {
+        return "Titular: " + getTitular() + " | Cantidad: " + getCantidad();
     }
-    
-    public void ingresar(double cantidad){
-        if (cantidad > 0){
-            this.setCantidad(this.getCantidad() + cantidad); 
-        }
-    }
-    
-    public void retirar(double cantidad){
-        if (cantidad > 0){
-            if(this.getCantidad()-cantidad>=0){
-                this.setCantidad(this.getCantidad() - cantidad);
-            }
-            
+
+    /**
+     * Metodo para ingresar cantidad
+     * @param cantidad cantidad de dinero ingresado
+     */
+    public void ingresar(double cantidad) {
+        if (cantidad > 0) {
+            this.setCantidad(this.getCantidad() + cantidad);
         }
     }
 
     /**
-     * @return the titular
+     * Metodo para retirar dinero
+     * @param cantidad cantidad de dinero a retirar
+     */
+    public void retirar(double cantidad) {
+        if (cantidad > 0) {
+            if (this.getCantidad() - cantidad >= 0) {
+                this.setCantidad(this.getCantidad() - cantidad);
+            }
+
+        }
+    }
+
+    /**
+     * Metodo devuelve al titular
+     * @return devuelve el titular de la cuenta
      */
     public String getTitular() {
         return titular;
     }
 
     /**
-     * @param titular the titular to set
+     * Metodo que estalece al titular de la cuenta
+     * @param titular Establece al titular de la cuenta.
      */
     public void setTitular(String titular) {
         this.titular = titular;
     }
 
     /**
-     * @return the cantidad
+     * Metodo que devuelve la cantidad de dinero en la cuenta.
+     * @return Devuelve la cantidad de dinero en la cuenta
      */
     public double getCantidad() {
         return cantidad;
     }
 
     /**
-     * @param cantidad the cantidad to set
+     * Metodo para establecer la cantidad de dinero en la cuenta.
+     * @param cantidad Establece la canditdad de dinero
      */
     public void setCantidad(double cantidad) {
         this.cantidad = cantidad;
--- src/iniciopoo/InicioPoo.java
+++ src/iniciopoo/InicioPoo.java
@@ -1,6 +1,17 @@
 package iniciopoo;
+
+/**
+ * Iniciamos simulación de cuentabancaria.
+ * @author sebas
+ * @version 1.0
+ * @since 24/05/2024
+ */
 public class InicioPoo {
 
+    /**
+     * Introducimos datos para simular cuenta bancaria.
+     * @param args
+     */
     public static void main(String[] args) {
         Cuenta primeraCuenta = new Cuenta("Paco Gerte", 2000.00);
         System.out.println(primeraCuenta);
